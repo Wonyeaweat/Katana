9dd0afd302b4a360f923d1d199aeae9aff3d9745
Wonyeaweat <47973479+Wonyeaweat@users.noreply.github.com>
2021-03-05 21:18:08 +0800

    Add files via upload

--- analyze.py
+++ analyze.py
@@ -5,8 +5,36 @@ import sys
 import os
 import pytesseract
 
-def init(inputImg):
-    if(not os.path.exists('./Analyze')):
+
+def imageOptimize(inputImg):
+    flag = True
+    img = np.array(cv2.resize(inputImg, (100, 100), cv2.INTER_CUBIC))  # 图片修改成100*100 px
+    ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)  # 将灰度图二值化
+    x1, x2, y1, y2 = 0, 99, 0, 99
+    while sum(img[x1]) < 255 * 30:  # 黑色像素>70个
+        x1 += 1
+    while sum(img[x2]) < 255 * 30:  # 黑色像素>70个
+        x2 -= 1
+    while sum(i[y1] for i in img) < 255 * 30:  # 黑色像素>70个
+        y1 += 1
+    while sum(i[y2] for i in img) < 255 * 30:  # 黑色像素>70个
+        y2 -= 1
+    img = cv2.resize(img[x1:x2, y1:y2], (100, 100), cv2.INTER_CUBIC)
+    cnt = sum([sum(i) for i in img])
+    if cnt > 2540000:
+        flag = False
+    # img = cv2.resize(img, (100, 100))  # 图片修改成100*100 px
+    return flag, img
+
+
+'''
+图片调整为100*100px后，如果某行的像素黑色数量超过70 认为是尚未去除干净的直线
+去除后，重新调整为100*100px 统计全部像素灰度值之和 如果超过2540000 认为是全白图片
+'''
+
+
+def solve(inputImg):
+    if not os.path.exists('./Analyze'):
         os.mkdir('./Analyze')
     img = inputImg
     grayImage = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)  # 将输入图片转成灰度图
@@ -14,11 +42,15 @@ def init(inputImg):
 
     x_avg = np.mean(binaryImage, 1)  # 压缩列，对每行求平均值
     y_avg = np.mean(binaryImage, 0)  # 压缩行，对每列求平均值
+    '''
+    对二值化后的图片而言，黑色直线表现为：灰度值接近0 其他空白处灰度值接近255
+    所以，对每行每列求平均值，如果平均值大，说明直线长度短，反之说明长
+    可以根据这个规律 判断出图片哪些位置有直线，直线的长度是多少
+    利用这个规律可以找到棋盘与方格的边界，统计出对应的数据
+    '''
 
     x = list()
     y = list()
-    # x,y两个list用于记录原图像素中哪些行/列是一整条直线
-
     for i in range(len(x_avg)):
         if x_avg[i] > 100:
             x_avg[i] = 255
@@ -29,10 +61,15 @@ def init(inputImg):
             y_avg[i] = 255
         elif abs(y_avg[i] - y_avg[i - 1]) > 100:
             y.append([i, y_avg[i]])
+    '''
+    遍历图片的每一行每一列，我们认为当该行/列 的平均灰度值大于100时，不是直线
+    反之，如果该行恰好是经过了棋盘左方或上方的数字，其平均灰度值也会较小，但并不是直线
+    所以，增加额外的判定条件：与上一行/列 的平均灰度值差值超过100，才是直线
+    并按 (像素坐标位置，平均灰度值) 的格式加入到x,y 两个list中
+    '''
+
     begX = 2
     begY = 2
-    # 计算从哪行哪列开始出现数字
-
     while begX < len(x) and abs(x[begX][1] - x[begX - 1][1]) < 20:
         begX += 1
     if begX >= len(x):
@@ -41,51 +78,77 @@ def init(inputImg):
         begY += 1
     if begY >= len(y):
         begY = 1
+    '''
+    Katana的棋盘左上方是预览区，所以经过预览区的直线比其他直线稍短，等价于平均灰度值有所提升
+    遍历x,y两个list，找到灰度值下降的第一条直线，其对应位置就是有数字开始的直线位置
+    分别将其对应的编号 保存在begX与begY中
+    存在一种特殊情况，即数字区只有一行/列，导致没有经过预览区的直线，全部直线都是一样长，
+    此时需要特殊判断处理
+    '''
 
     dataAnalyze = open("./Analyze/data.dat", "w")
-    dataAnalyze.write(str(len(x) - begX - 1) + ' ' + str(len(y) - begY - 1) + '\n')
-    # 输出棋盘大小 几行几列
+    height = len(x) - begX - 1
+    width = len(y) - begY - 1
+    dataAnalyze.write(str(height) + ' ' + str(width) + '\n')
+    '''
+    将分析结果保存在 /Analyze/data.dat 文件下。该文件第一行两个数字即棋盘大小（格子数*格子数）
+    '''
 
     for i in range(begX, len(x) - 1):
         outputX = list()
         for j in range(begY):
-            tmpImg = binaryImage[
-                     x[i][0] + math.ceil((x[i + 1][0] - x[i][0]) / 10.0):x[i + 1][0],
-                     y[j][0] + math.ceil((y[j + 1][0] - y[j][0]) / 10.0):y[j + 1][0]]
-            # 去掉黑边，采用-10%的方法
-            fileName = "./Analyze/X-" + str(i - begX + 1) + "-" + str(j + 1) + ".png"
-            cv2.imwrite(fileName, tmpImg)
-            text = str.strip(
-                pytesseract.image_to_string(fileName, config='--psm 10 -c tessedit_char_whitelist=0123456789'))+' '
-            # --psm 10 指将图片识别成单个字符 后面白名单也有助于提升识别率
-            if(text != ' '):
+            tmpImg = binaryImage[x[i][0]:x[i + 1][0], y[j][0]:y[j + 1][0]]  # 截取了一个方格的图片
+            flag, tmpImg = imageOptimize(tmpImg)  # 判断图片是否有数字（可能为空），处理后的图片
+            if (flag):  # 有数字
+                fileName = "./Analyze/X-" + str(i - begX + 1) + "-" + str(j + 1) + ".png"
+                cv2.imwrite(fileName, tmpImg)
+                text = str.strip(
+                    pytesseract.image_to_string(fileName, config='--psm 7 -c tessedit_char_whitelist=0123456789')) + ' '
                 outputX.append(text)
+                print('X', i - begX + 1, j + 1, text)
             # 存在空白图片，识别出来为空，需要特殊判断
-        dataAnalyze.write(str(len(outputX))+' ')
+        dataAnalyze.write(str(len(outputX)) + ' ')
         dataAnalyze.writelines(outputX)
         dataAnalyze.write('\n')
     # 每行先输出当前行的数字个数，接着空格隔开，从左至右输出数字
+    '''
+    将数字区进行分割，交给tesseract 进行数字识别。利用之前统计的直线数据，可以很方便截取一个个方格。
+    这段程序是分割图片左侧的数字区域，for i 枚举的是每行的上方直线，for j枚举的是每列的左侧直线
+    tmpImg截取后交由 imageOptimize 进行处理（判断有无数字，去掉边框等）
+    '''
 
     for j in range(begY, len(y) - 1):
         outputY = list()
         for i in range(begX):
-            tmpImg = binaryImage[
-                     x[i][0] + math.ceil((x[i + 1][0] - x[i][0]) / 10.0):x[i + 1][0],
-                     y[j][0] + math.ceil((y[j + 1][0] - y[j][0]) / 10.0):y[j + 1][0]]
-            fileName = "./Analyze/Y-" + str(j - begY + 1) + "-" + str(i + 1) + ".png"
-            cv2.imwrite(fileName, tmpImg)
-            text = str.strip(
-                pytesseract.image_to_string(fileName, config='--psm 10 -c tessedit_char_whitelist=0123456789'))+' '
-            if(text!=' '):
+            tmpImg = binaryImage[x[i][0]:x[i + 1][0], y[j][0]:y[j + 1][0]]
+            flag, tmpImg = imageOptimize(tmpImg)
+            if flag:
+                fileName = "./Analyze/Y-" + str(j - begY + 1) + "-" + str(i + 1) + ".png"
+                cv2.imwrite(fileName, tmpImg)
+                text = str.strip(
+                    pytesseract.image_to_string(fileName, config='--psm 7 -c tessedit_char_whitelist=0123456789')) + ' '
                 outputY.append(text)
+                print('Y', j - begY + 1, i + 1, text)
         dataAnalyze.write(str(len(outputY)) + ' ')
         dataAnalyze.writelines(outputY)
         dataAnalyze.write('\n')
     # 每行先输出当前列的数字个数，接着空格隔开，从上至下输出数字
-    print ('Finished.')
+    print('Solving...')
+    os.system('solve.exe')
+    res = list()
+    with open('./Analyze/result.dat', 'r') as file:
+        lines = file.readlines()
+        for i in lines:
+            res.append(list(filter(None, i.split(" "))))
+    img = inputImg
+    for i in range(height):
+        for j in range(width):
+            if res[i][j]=='1':
+                img[x[begX+i][0]:x[begX+i+1][0],y[begY+j][0]:y[begY+j+1][0]] = [0,0,0]
+    cv2.imwrite('./Analyze/result.png',img)
 
 
 if __name__ == '__main__':
-    filePath = './test_images/test_1.PNG'
+    filePath = './test_images/test_2.PNG'
     img = cv2.imread(filePath)
-    init(img)
+    solve(img)
--- solve.cpp
+++ solve.cpp
@@ -1,7 +1,128 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
 using namespace std;
+int n, m;
+int x[200][200], y[200][200];
+int res[200][200]={0};
+void init(){
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; i++){
+        scanf("%d", &x[i][0]);
+        for (int j = 1; j <= x[i][0]; j++)
+			scanf("%d", &x[i][j]);
+    }
+    for (int i = 1; i <= m; i++){
+        scanf("%d", &y[i][0]);
+        for (int j = 1; j <= y[i][0]; j++)
+            scanf("%d", &y[i][j]);
+    }
+}
+
+bool check(int row,int col){
+	//检查第col列,匹配到第row行结束 
+	//即检查(row,col)位置的方块是否合法 
+	int i=1,match=1;
+	while(i<=row){
+		int cnt = 0;
+		while(i<=row && res[i][col]==0) i++;
+		while(i<=row && res[i][col]==1) cnt++,i++; // 搜索了一段连续区间 
+		if(cnt>0 && match>y[col][0]) return false;
+		if(i>row){
+			if(cnt>y[col][match]) return false;
+			return true;
+		} else {
+			if(cnt!=y[col][match]) return false;
+			else match++;
+		}
+	}
+	return true;
+}
+
+void output(){
+	//printf("%d %d\n",n,m);
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			printf("%d ",res[i][j]);
+			//if(j%5==0) printf(" ");
+		}
+		//if(i%5==0) printf("\n");
+		printf("\n");
+	}
+	printf("\n");
+}
+
+
+void dfs(int row,int id,int beg){
+	//按行搜索,当前在考虑第row行第id段 可摆放位置从beg开始 
+	//尝试每个连续段所摆放的位置
+	//printf("row=%d,id=%d,beg=%d\n",row,id,beg);
+	if(row>n){
+		bool flag = true;
+		for(int i=1;i<=m;i++) flag &= check(row,i);
+		if(flag == true) output();//认为找到了一组解
+		return ; 
+	}
+	int tot_left = 0;
+	for(int i=id;i<=x[row][0];i++)
+		tot_left += x[row][i];
+	tot_left+=x[row][0]-id; 
+		//至少要tot_left个位置才能放下剩下的所有段
+	int i=beg;
+	while(i<=m-tot_left+1){
+		//枚举放置位置 
+		int j=0,next;
+		bool flag=true;
+		int k=beg;
+		for(;j<x[row][id];j++){
+			res[row][i+j]=1;
+			bool fflag=true;
+	     //   if(row==5)output();
+			while(k<=i+j && k<=m){
+				if(check(row,k)==false){
+		//			printf("check(%d,%d)=false\n",row,k);
+				 	if(k<i) {
+				 		//在(row,k)这个位置,填或不填 都会产生错误
+						//这等价于说 是之前行产生了错误 但尚未识别
+						while(j>=0){ res[row][i+j]=0; j--; }
+						return ;	
+					}
+				 	else {fflag=false;break;}
+				}
+				k++;
+				//while循环结束后k停在i+j+1位置
+				//当本段未填完时，k是下一个所填位置
+				//当本段已填完时，k是空格位置，仍需一次检查 
+			}
+			if(fflag==false){flag = false;break;}
+			//这意味着 在i这个起始点往后填第j格(k=i+j)时出现错误 
+			//下一次起点应该为k+1 因为k之前作为起点都必然覆盖到k 
+		}
+		if(flag==false){ 
+			//在填写过程中出现错误 
+			while(j>=0){ res[row][i+j]=0; j--; }
+		//	printf("\n===\n");
+			i++;
+		} else {
+			if(check(row,i+j)==false) {
+			//	printf("check(%d,%d)=false\n",row,i+j);
+				while(j>=0){ res[row][i+j]=0; j--; }
+				//填写没有错误，但每段之间必要空格，该空格导致错误 
+				i++;
+			} else {
+				if(id >= x[row][0]) dfs(row+1,1,1);
+				else dfs(row,id+1,i+j+1);
+				while(j>=0){ res[row][i+j]=0; j--; }
+				i++;	
+			}
+		}
+	} 
+}
+
 int main(){
-    ifstream
+	freopen("./Analyze/data.dat", "r", stdin);
+	freopen("./Analyze/result.dat","w",stdout);
+	init();
+	dfs(1,1,1);
     return 0;
 }
